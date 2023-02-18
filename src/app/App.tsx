b1d48dfe5d032422e069e8ec299a961cfe24ecc3
artyomromaniv <romaniv.artyom8@gmail.com>
2023-02-18 16:33:15 +0300

    less 16 - formik

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -12,6 +12,8 @@ import Container from '@mui/material/Container';
 import LinearProgress from '@mui/material/LinearProgress';
 import {Menu} from '@mui/icons-material';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Login} from "../features/Login/Login";
+import {Navigate, Route, Routes} from "react-router-dom";
 
 
 function App() {
@@ -32,7 +34,12 @@ function App() {
                 {status === 'loading' && <LinearProgress/>}
             </AppBar>
             <Container fixed>
-                <TodolistsList/>
+                <Routes>
+                    <Route path={'/'} element={<TodolistsList/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/404'} element={<h1>404: PAGE NOT FOUND</h1>} />
+                    <Route path={'/*'} element={<Navigate to='/404'/>}/>
+                </Routes>
             </Container>
         </div>
     )
---
+++ src/features/Login/Login.tsx
@@ -0,0 +1,72 @@
+import React from 'react'
+import Grid from '@mui/material/Grid';
+import Checkbox from '@mui/material/Checkbox';
+import FormControl from '@mui/material/FormControl';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import FormLabel from '@mui/material/FormLabel';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import {useFormik} from "formik";
+
+export const Login = () => {
+
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false,
+        },
+        onSubmit: values => {
+            alert(JSON.stringify(values, null, 2));
+        }
+    });
+
+    return <Grid container justifyContent={'center'}>
+        <Grid item justifyContent={'center'}>
+            <FormControl>
+                <form onSubmit={formik.handleSubmit}>
+                    <FormLabel>
+                        <p>To log in get registered
+                            <a href={'https://social-network.samuraijs.com/'}
+                               target={'_blank'}> here
+                            </a>
+                        </p>
+                        <p>or use common test account credentials:</p>
+                        <p>Email: free@samuraijs.com</p>
+                        <p>Password: free</p>
+                    </FormLabel>
+                    <FormGroup>
+
+                        <TextField
+                            label="Email"
+                            margin="normal"
+                            name='email'
+                            onChange={formik.handleChange}
+                            value={formik.values.email}
+                        />
+                        <TextField
+                            type="password"
+                            label="Password"
+                            margin="normal"
+                            name='password'
+                            onChange={formik.handleChange}
+                            value={formik.values.password}
+                        />
+                        <FormControlLabel label={'Remember me'} control={
+                            <Checkbox
+                                name='rememberMe'
+                                onChange={formik.handleChange}
+                                checked={formik.values.rememberMe}/>
+                        }/>
+                        <Button type={'submit'} variant={'contained'} color={'primary'}>
+                            Login
+                        </Button>
+
+                    </FormGroup>
+                </form>
+            </FormControl>
+        </Grid>
+    </Grid>
+}
